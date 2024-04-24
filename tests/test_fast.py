902e070e6963fa41b9b1c2731fdb467eec0bf426
arsmlnkv <melnikov.arsene@gmail.com>
2024-04-24 15:40:54 +0200

    Modified tests so they can be used with pytest and automated.

--- tests/test_fast.py
+++ tests/test_fast.py
@@ -5,11 +5,21 @@ Created on Wed Apr  3 14:24:32 2024
 @author: AA000139
 """
 
-from ..sep005_io_fast.fast import read_fast_file
+from pathlib import Path
+import pytest
+
+from sep005_io_fast.fast import read_fast_file
 from sdypy_sep005.sep005 import assert_sep005
 
-file_path = '5MW_OC4Semi_H2.5_T10_WS13.out' # Path to the FAST.out file of interest
 
-signals = read_fast_file(file_path)
-assert_sep005(signals)
+@pytest.fixture(scope='module')
+def file_path():
+    filename = "5MW_OC4Semi_H2.5_T10_WS13.out"
+    pathname = Path(__file__).resolve().parent
+    return pathname / filename  # Path to the FAST.out file of interest
+
+
+def test_read_fast(file_path):
+    signals = read_fast_file(file_path)
+    assert assert_sep005(signals) is None
 
