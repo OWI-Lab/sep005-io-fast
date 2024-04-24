0194bae27d9475fd92f7cb97da3cbe7571c024f2
arsmlnkv <melnikov.arsene@gmail.com>
2024-04-24 17:06:10 +0200

    Dynamic versioning and added script for releasing.

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+version_old=$(git describe --tags --abbrev=0)
+if [ "$version_old" = "$1" ]; then
+    echo "Warning: you did not change the version! Please check the __version__ in your __init__.py"
+    exit 1
+fi
+echo "Creating new release: $version_old -> $1"
+git add .
+git commit -m "Bumpversion $version_old -> $1"
+git push
+git tag -a "$1" -m "Release $1"
+git push origin "$1"
