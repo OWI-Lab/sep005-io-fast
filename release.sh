2aa378df7a3cbc1ba39f0001bb63203dca4fd591
arsmlnkv <melnikov.arsene@gmail.com>
2024-04-25 09:50:16 +0200

    Bumpversion  -> v0.0.1

--- release.sh
+++ release.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 version_old=$(git describe --tags --abbrev=0)
-if [ "$version_old" == "$1" ]; then
+if [ "$version_old" == "v$1" ]; then
     echo "Error: you did not change the version! Please check the __version__ in your __init__.py"
     exit 1
 fi
-echo "Creating new release: $version_old -> $1"
+echo "Creating new release: $version_old -> v$1"
 git add .
-git commit -m "Bumpversion $version_old -> $1"
+git commit -m "Bumpversion $version_old -> v$1"
 git push
-git tag -a "$1" -m "Release $1"
-git push origin "$1"
+git tag -a "v$1" -m "Release v$1"
+git push origin "v$1"
